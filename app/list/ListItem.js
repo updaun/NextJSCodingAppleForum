8cab66c310d48413537758237e109fe1537a48de
updaun <updauney@daum.net>
2024-06-25 21:18:00 +0900

    feat: delete animation

--- app/list/ListItem.js
+++ app/list/ListItem.js
@@ -8,7 +8,7 @@ export default function ListItem({result}) {
           <div className="list-item" key={i}>
             <Link href={'/detail/' + result[i]._id}>{result[i].title}</Link>
             <Link href={'/edit/' + result[i]._id} className="list-btn">✏️</Link>
-            <span onClick={() => {
+            <span onClick={(e) => {
               fetch('/api/post/delete', {
                 method: 'DELETE',
                 headers: {
@@ -17,6 +17,10 @@ export default function ListItem({result}) {
                 body: JSON.stringify({_id: result[i]._id}),
               })
               .then((r) => {
+                e.target.parentElement.style.opacity = 0
+                setTimeout(() => {
+                  e.target.parentElement.style.display = 'none'
+                }, 1000)
                 return r.json()
               })
               .then((result) => {
