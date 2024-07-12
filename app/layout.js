77b96433e3932100707d3e3a000243b04fd6f278
updaun <updauney@daum.net>
2024-07-12 17:12:38 +0900

    feat: dark mode

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
-import { Inter, Signika_Negative } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
 import LoginBtn from "./LoginBtn";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import DarkMode from "./DarkMode";
+import { cookies } from "next/headers";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -14,16 +14,20 @@ export const metadata = {
 
 export default async function RootLayout({ children }) {
   let session = await getServerSession(authOptions)
-  console.log(session)
+
+  let res = cookies().get('mode')
+  console.log(res)
+
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={res != undefined && res.value == 'dark' ? "dark-mode" : ""}>
       <div className="navbar"> 
         <Link href="/" className="logo">Appleforum</Link> 
         <Link href="/list">List</Link>
         {/* 3항 연산자로 로그인 로그아웃 버튼 */}
         {session?.user ? <Link href="/write">Write</Link> : null}
         {session?.user ? <Link href="/api/auth/signout">Signout</Link> : <LoginBtn/>}
+        <DarkMode/>
       </div>  
         {children}
       </body>
